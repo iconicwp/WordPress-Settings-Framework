52c2974e27566042d72558b891aa3f79dea4468e
James Kemp <me@jckemp.com>
2015-10-04 15:30:54 +0100

    [version]
    New version to reflect the new settings methods

--- wpsf-test.php
+++ wpsf-test.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Settings Framework Example
 Description: An example of the WP Settings Framework in action.
-Version: 1.4.0
+Version: 1.6.0
 Author: Gilbert Pellegrom
 Author URI: http://dev7studios.com
 */
