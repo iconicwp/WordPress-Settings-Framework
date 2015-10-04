914f78d1c926a556c3c99cd39c6a94f05cc01b22
James Kemp <me@jckemp.com>
2015-10-04 14:07:52 +0100

    [instances]
    Implement new settings page method

--- wpsf-test.php
+++ wpsf-test.php
@@ -15,52 +15,32 @@ class WPSFTest {
     function __construct()
     {
         $this->plugin_path = plugin_dir_path( __FILE__ );
-        add_action( 'admin_menu', array(&$this, 'admin_menu'), 99 );
+        add_action( 'admin_menu', array( $this, 'init_settings' ) );
 
         // Include and create a new WordPressSettingsFramework
         require_once( $this->plugin_path .'wp-settings-framework.php' );
         $this->wpsf = new WordPressSettingsFramework( $this->plugin_path .'settings/example-settings.php', 'my_example_settings' );
+        
         // Add an optional settings validation filter (recommended)
         add_filter( $this->wpsf->get_option_group() .'_settings_validate', array(&$this, 'validate_settings') );
     }
 
-    function admin_menu()
-    {
-        $page_hook = add_menu_page( __( 'WPSF', 'wp-settings-framework' ), __( 'WPSF', 'wp-settings-framework' ), 'update_core', 'wpsf', array(&$this, 'settings_page') );
-        add_submenu_page( 'wpsf', __( 'Settings', 'wp-settings-framework' ), __( 'Settings', 'wp-settings-framework' ), 'update_core', 'wpsf', array(&$this, 'settings_page') );
+    public function init_settings() {
+        
+        $this->wpsf->add_settings_page( array(
+            'parent_slug' => 'options-general.php',
+            'page_title'  => __( 'WPSF Settings' ),
+            'menu_title'  => __( 'WPSF' )
+        ) );
+        
     }
 
-    function settings_page()
-	{
-	    // Your settings page
-	    ?>
-		<div class="wrap">
-			<div id="icon-options-general" class="icon32"></div>
-			<h2>WP Settings Framework Example</h2>
-			<?php
-			// Output your settings form
-			$this->wpsf->settings();
-			?>
-		</div>
-		<?php
-
-		// Get settings
-		//$settings = wpsf_get_settings( 'my_example_settings' );
-		//echo '<pre>'.print_r($settings,true).'</pre>';
-
-		// Get individual setting
-		//$setting = wpsf_get_setting( 'my_example_settings', 'general', 'text' );
-		//var_dump($setting);
-	}
-
-	function validate_settings( $input )
-	{
-	    // Do your settings validation here
-	    // Same as $sanitize_callback from http://codex.wordpress.org/Function_Reference/register_setting
+    function validate_settings( $input ) {
+    	// Do your settings validation here
+	// Same as $sanitize_callback from http://codex.wordpress.org/Function_Reference/register_setting
     	return $input;
-	}
+    }
 
 }
-new WPSFTest();
 
-?>
+$wpsf_test = new WPSFTest();
