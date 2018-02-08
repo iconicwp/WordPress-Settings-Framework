2e3c25f4757f4dd3e7a4e9d19654cde3d1a7b7c5
James Kemp <me@jckemp.com>
2018-02-08 20:52:24 +0000

    Update test file.

--- wpsf-test.php
+++ wpsf-test.php
@@ -8,39 +8,57 @@ Author URI: http://dev7studios.com
 */
 
 class WPSFTest {
+	/**
+	 * @var string
+	 */
+	private $plugin_path;
 
-    private $plugin_path;
-    private $wpsf;
+	/**
+	 * @var WordPressSettingsFramework
+	 */
+	private $wpsf;
 
-    function __construct()
-    {
-        $this->plugin_path = plugin_dir_path( __FILE__ );
-        add_action( 'admin_menu', array( $this, 'init_settings' ), 99 );
+	/**
+	 * WPSFTest constructor.
+	 */
+	public function __construct() {
+		$this->plugin_path = plugin_dir_path( __FILE__ );
 
-        // Include and create a new WordPressSettingsFramework
-        require_once( $this->plugin_path .'wp-settings-framework.php' );
-        $this->wpsf = new WordPressSettingsFramework( $this->plugin_path .'settings/example-settings.php', 'my_example_settings' );
-        
-        // Add an optional settings validation filter (recommended)
-        add_filter( $this->wpsf->get_option_group() .'_settings_validate', array(&$this, 'validate_settings') );
-    }
+		// Include and create a new WordPressSettingsFramework
+		require_once( $this->plugin_path . 'wp-settings-framework.php' );
+		$this->wpsf = new WordPressSettingsFramework( $this->plugin_path . 'settings/settings-general.php', 'prefix_settings_general' );
 
-    public function init_settings() {
-        
-        $this->wpsf->add_settings_page( array(
-            'parent_slug' => 'options-general.php',
-            'page_title'  => __( 'WPSF Settings' ),
-            'menu_title'  => __( 'WPSF' )
-        ) );
-        
-    }
+		// Add admin menu
+		add_action( 'admin_menu', array( $this, 'add_settings_page' ), 20 );
+		
+		// Add an optional settings validation filter (recommended)
+		add_filter( $this->wpsf->get_option_group() . '_settings_validate', array( &$this, 'validate_settings' ) );
+	}
 
-    function validate_settings( $input ) {
-    	// Do your settings validation here
-	// Same as $sanitize_callback from http://codex.wordpress.org/Function_Reference/register_setting
-    	return $input;
-    }
+	/**
+	 * Add settings page.
+	 */
+	public function settings_page() {
+		$this->wpsf->add_settings_page( array(
+			'parent_slug' => 'woocommerce',
+			'page_title'  => __( 'Page Title', 'text-domain' ),
+			'menu_title'  => __( 'menu Title', 'text-domain' ),
+			'capability'  => 'manage_woocommerce',
+		) );
+	}
 
+	/**
+	 * Validate settings.
+	 * 
+	 * @param $input
+	 *
+	 * @return mixed
+	 */
+	public function validate_settings( $input ) {
+		// Do your settings validation here
+		// Same as $sanitize_callback from http://codex.wordpress.org/Function_Reference/register_setting
+		return $input;
+	}
 }
 
 $wpsf_test = new WPSFTest();
